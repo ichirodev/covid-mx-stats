b786285858dd5be07edb90106b01ce0954e5fab7
ichirodev <adanschiffer@hotmail.com>
2022-05-02 21:30:12 -0500

    TopBar added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LogInPage from './components/pages/LogInPage';
 import RegisterPage from './components/pages/RegisterPage';
 import HomePage from './components/pages/HomePage';
+import DashboardPage from './components/pages/DashboardPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LogInPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </Router>
   );
